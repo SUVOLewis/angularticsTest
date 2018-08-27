62fc1c546a6fe85db45dcab7f5b31354e6a326cd
SUVOLewis <lewis.kosztan@suvotechnology.com>
2018-08-27 18:59:40 +0100

    contains correct files, still not working

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Angulartics2 } from 'angulartics2';
+
+import { NgModule } from '@angular/core';
+import { Angulartics2Module } from 'angulartics2';
+
+@NgModule({
+  imports: [
+    Angulartics2Module,
+  ]
+})
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  test = 'Test';
+
+  constructor(private angulartics2: Angulartics2) {
+    this.test = 'Test2';
+
+    this.angulartics2.eventTrack.next({
+      action: 'aboutClicked',
+      properties: { category: 'testCategory'}
+    });
+   }
+
+  ngOnInit() {
+
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
+import { RouterModule, Routes} from '@angular/router';
+
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'about', component: AboutComponent},
+      {path: 'news', component: NewsComponent}
+    ]),
+    Angulartics2Module.forRoot([Angulartics2GoogleAnalytics])
   ],
   providers: [],
   bootstrap: [AppComponent]
